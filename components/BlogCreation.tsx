0e62c998ed244f6fce2e65a6228833ca5abdb176
zarif007 <zarifhuq786@gmail.com>
2022-08-17 17:48:42 +0600

    fixed title

--- components/BlogCreation.tsx
+++ components/BlogCreation.tsx
@@ -14,13 +14,13 @@ const modules = {
     [{ header: '1' }, { header: '2' }, { font: [] }],
     [{ size: [] }],
     ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+    ['image'],
     [
       { list: 'ordered' },
       { list: 'bullet' },
       { indent: '-1' },
       { indent: '+1' },
     ],
-    ['image'],
     ['clean'],
   ],
   clipboard: {
@@ -46,17 +46,37 @@ const formats = [
 
 const BlogCreation = () => {
 
-  const [value, setValue] = useState('');
+  const [content, setContent] = useState<{ title: String, body: string }>({title: 'title', body: ''});
 
-  useEffect(() => {
-    console.log(value)
-  }, [value])
+  const [showTitleInput, setShowTitleInpt] = useState<boolean>(false);
 
   return (
     <div className='text-white text-lg'>
-      Blog
+      {
+        !showTitleInput ? <div className='w-full border border-black py-3 bg-black text-2xl font-bold my-4 px-1 cursor-pointer'
+          onClick={() => setShowTitleInpt(true)}>
+          {content.title}
+        </div> :
+        <input 
+          className='border w-full py-3 bg-black text-xl font-bold my-4 px-1'
+          placeholder={`${content.title}`}
+          defaultValue={`${content.title}`}
+          onChange={(e: any) => {
+            const updated = content;
+            updated.title = e.target.value;
+            setContent(updated);
+          }}
+          onMouseOut={() => setShowTitleInpt(false)}
+        />
+      }
+
+      <p className='my-2 text-lg font-bold text-gray-300'>Content</p>
       <div className='mb-32 mt-2'>
-        <QuillNoSSRWrapper theme='snow' placeholder={value} modules={modules} formats={formats} onChange={setValue} />
+        <QuillNoSSRWrapper className='' theme='snow' placeholder={content.body} modules={modules} formats={formats} onChange={(e: any) => {
+          const updated = content;
+          updated.body = e;
+          setContent(updated);
+        }} />
       </div>
     </div>
   )
