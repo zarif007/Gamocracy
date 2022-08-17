7ce2ef817d10f092fbf36b79f48749a351b2215f
zarif007 <zarifhuq786@gmail.com>
2022-08-18 00:36:14 +0600

    Fixed content part of blog

--- components/BlogCreation.tsx
+++ components/BlogCreation.tsx
@@ -3,6 +3,9 @@ import React, { useEffect, useState } from 'react'
 import '../node_modules/react-quill/dist/quill.snow.css'
 
 
+import DOMPurify from 'isomorphic-dompurify';
+
+
 const QuillNoSSRWrapper = dynamic(import('react-quill'), {	
   ssr: false,
   loading: () => <p>Loading ...</p>,
@@ -46,37 +49,38 @@ const formats = [
 
 const BlogCreation = () => {
 
-  const [content, setContent] = useState<{ title: String, body: string }>({title: 'title', body: ''});
+  const [content, setContent] = useState<{ title: String, body: string }>({title: '', body: ''});
 
-  const [showTitleInput, setShowTitleInpt] = useState<boolean>(false);
+  const [showTitleBorder, setShowTitleBorder] = useState<boolean>(false);
 
   return (
     <div className='text-white text-lg'>
-      {
-        !showTitleInput ? <div className='w-full border border-black py-3 bg-black text-2xl font-bold my-4 px-1 cursor-pointer'
-          onClick={() => setShowTitleInpt(true)}>
-          {content.title}
-        </div> :
-        <input 
-          className='border w-full py-3 bg-black text-xl font-bold my-4 px-1'
-          placeholder={`${content.title}`}
-          defaultValue={`${content.title}`}
+      <input 
+          className={`border ${!showTitleBorder && 'border-black'} ${content.title === '' && 'border-white'} w-full py-3 bg-black text-2xl font-bold my-4 px-1`}
+          placeholder='Title'
           onChange={(e: any) => {
             const updated = content;
             updated.title = e.target.value;
             setContent(updated);
           }}
-          onMouseOut={() => setShowTitleInpt(false)}
+          onMouseOut={() => setShowTitleBorder(false)}
+          onClick={() => setShowTitleBorder(true)}
         />
-      }
 
       <p className='my-2 text-lg font-bold text-gray-300'>Content</p>
-      <div className='mb-32 mt-2'>
+      <div className='mb-20 mt-2'>
         <QuillNoSSRWrapper className='' theme='snow' placeholder={content.body} modules={modules} formats={formats} onChange={(e: any) => {
           const updated = content;
           updated.body = e;
           setContent(updated);
         }} />
+      </div>
+
+      {/* { <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(content.body) }} /> } */}
+
+      <div className='flex space-x-2 justify-end'>
+        <button className='py-1 px-3 rounded-md border-2 border-[#DC143C] text-lg font-semibold text-[#DC143C]'>Preview</button>
+        <button className='py-1 px-6 rounded-md bg-[#DC143C] text-lg font-semibold text-white'>Post</button>
       </div>
     </div>
   )
