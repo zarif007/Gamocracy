c69806112056a962e60173c7492b0fdc7cb0f0d2
zarif007 <zarifhuq786@gmail.com>
2022-10-18 00:22:07 +0600

    Adding Emoji on post part

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,7 +1,9 @@
-import React from "react";
-import postInterface from "../../Interfaces/PostInterface";
+import React, { useEffect, useState } from "react";
+import EmojiPicker from 'emoji-picker-react';
 import NavBar from "../../components/navBars/NavBar";
 import Slider from "../../components/reusable/Slider";
+import { Theme } from 'emoji-picker-react';
+import { MdOutlineAddCircle } from "react-icons/md";
 
 const post: any = {
   content: `Today, zekken has already made a name for himself as one of the brightest spots on the XSET roster that came from behind to become arguably the second-best team in North America. At Valorant Champions 2022, they also showed up against some of the strongest international competition, taking down FunPlus Phoenix and Fnatic.
@@ -19,15 +21,50 @@ const post: any = {
 
 const Post = () => {
 
+  const [showEmojiPicker, setShowEmojiPicker] = useState<boolean>(false);
+
+  const [selectedEmojies, setSelectedEmojies] = useState<any>({});
+
+  const [emojiesArrayfied, setEmojiesArrayfied] = useState<any>([]);
+
+  const addEmoji = (e: any) => {
+    const updated = selectedEmojies;
+    updated[`${e.emoji}`] = updated[`${e.emoji}`] === undefined ? 1 : updated[`${e.emoji}`] + 1;
+    setSelectedEmojies(updated);
+    !emojiesArrayfied.includes(e.emoji) && setEmojiesArrayfied([ ...emojiesArrayfied, e.emoji ]);
+  }
+
   return (
     <div className="">
       <NavBar />
-      <div className="bg-[#121212] mt-4 rounded-md mb-20 md:mb-4 border-2 border-[#DC143C] max-w-5xl mx-auto">
+      <div className="mt-4 mb-20 md:mb-12  max-w-5xl mx-auto">
         
-        <Slider images={post.images} />
-        <div className="my-4 text-3xl md:text-5xl font-bold text-[#DC143C] mx-2">{post.title}</div>
-        <div className="my-4 text-lg md:text-xl font-semibold text-gray-300 mx-2">{post.content}</div>
-      </div>
+        <div className="bg-[#121212] border-2 border-[#DC143C] rounded-md">
+          <Slider images={post.images} />
+          <div className="my-4 text-3xl md:text-5xl font-bold text-[#DC143C] mx-2">{post.title}</div>
+          <div className="my-4 text-lg md:text-xl font-semibold text-gray-300 mx-2 mb-8">{post.content}</div>
+        </div>
+
+        <div className="flex space-x-2 -mt-6 ml-2">
+          {
+            emojiesArrayfied.map((emoji: any, index: number) => {
+                return (
+                  <span key={index} className="">
+                    <p className="w-10 h-10 text-3xl cursor-pointer rounded-full bg-red-500">{ emoji }</p>
+                    <p>{ selectedEmojies[emoji] }</p>
+                  </span>
+                )
+              })
+          }
+
+          <div>
+            <MdOutlineAddCircle className="text-gray-200 bg-zinc-800 rounded-full w-10 h-10 p-1 cursor-pointer" onClick={() => setShowEmojiPicker(!showEmojiPicker)} />
+            {
+              showEmojiPicker && <EmojiPicker onEmojiClick={(e) => addEmoji(e)} theme={Theme.DARK} />
+            }
+          </div>
+        </div>
+      </div>      
     </div>
   );
 };
