64ac7b5c3b47388379dd22df8112af88ecd9a78c
zarif007 <zarifhuq786@gmail.com>
2022-10-24 23:32:00 +0600

    Added post func

--- components/postComponents/PostContent.tsx
+++ components/postComponents/PostContent.tsx
@@ -11,7 +11,6 @@ import { showNotification } from "../../pages/_app";
 import EmojiPickerModal from "../modals/EmojiPickerModal";
 import Slider from "../reusable/Slider";
 
-
 const PostContent: React.FC<{ post: postInterface }> = ({ post }) => {
   const { data: session } = useSession();
 
@@ -22,7 +21,7 @@ const PostContent: React.FC<{ post: postInterface }> = ({ post }) => {
 
   useEffect(() => {
     post.reactions !== null && setReactions(post.reactions);
-  }, [post])
+  }, [post]);
 
   const addEmoji = (e: string) => {
     if (!session?.user?.email) {
@@ -51,29 +50,33 @@ const PostContent: React.FC<{ post: postInterface }> = ({ post }) => {
       ? storedReactors.length === 1
         ? setReactions([...reactions])
         : setReactions([
-          ...reactions,
-          {
-            emoji: e,
-            reactors: [
-              ...storedReactors.filter((x) => x !== session.user?.email),
-            ],
-          },
-        ])
+            ...reactions,
+            {
+              emoji: e,
+              reactors: [
+                ...storedReactors.filter((x) => x !== session.user?.email),
+              ],
+            },
+          ])
       : storedReactors.length
-        ? setReactions([
+      ? setReactions([
           ...reactions,
           {
             emoji: e,
             reactors: [...storedReactors, session?.user?.email || ""],
           },
         ])
-        : setReactions([
+      : setReactions([
           ...reactions,
           { emoji: e, reactors: [session?.user?.email || ""] },
         ]);
-    console.log({ ...post, reactions })
-    axios.put(`${apiEndpoints.post}/?postId=${post.postId}`, { ...post, reactions })
-        .then(res => console.log(res))
+    console.log({ ...post, reactions });
+    axios
+      .put(`${apiEndpoints.post}/?postId=${post.postId}`, {
+        ...post,
+        reactions,
+      })
+      .then((res) => console.log(res));
   };
   return (
     <div>
@@ -83,44 +86,43 @@ const PostContent: React.FC<{ post: postInterface }> = ({ post }) => {
           {post.title}
         </div>
         <div className="my-4 text-lg md:text-xl font-semibold text-gray-300 mx-2 mb-8">
-          {
-            post.content.split('\n').map((ps: string, index) => {
-              return (
-                <div key={index}>{ps}</div>
-              )
-            })
-          }
+          {post.content.split("\n").map((ps: string, index) => {
+            return <div key={index}>{ps}</div>;
+          })}
         </div>
       </div>
 
       <div className="flex space-x-2 -mt-6 ml-2 flex-wrap">
-        {reactions && reactions
-          .sort((a, b) => {
-            if (a.reactors.length > b.reactors.length) return -1;
-            else if (a.reactors.length < b.reactors.length) return 1;
-            return 0;
-          }).slice(0, Math.max(5, reactions.length))
-          .map((reaction: postReactionInterface, index: number) => {
-            return (
-              <span
-                key={index}
-                className="flex justify-center items-center flex-col"
-              >
-                <div
-                  className={`w-10 h-10 text-3xl cursor-pointer rounded-md  mx-auto hover:bg-[#DC143C] ${reaction.reactors.includes(session?.user?.email || "")
-                      ? " bg-[#DC143C]"
-                      : "bg-gray-800"
-                    }`}
-                  onClick={() => addEmoji(reaction.emoji)}
+        {reactions &&
+          reactions
+            .sort((a, b) => {
+              if (a.reactors.length > b.reactors.length) return -1;
+              else if (a.reactors.length < b.reactors.length) return 1;
+              return 0;
+            })
+            .slice(0, Math.max(5, reactions.length))
+            .map((reaction: postReactionInterface, index: number) => {
+              return (
+                <span
+                  key={index}
+                  className="flex justify-center items-center flex-col"
                 >
-                  {reaction.emoji}
-                </div>
-                <p className="text-white -mt-3 ml-6">
-                  {reaction.reactors.length}
-                </p>
-              </span>
-            );
-          })}
+                  <div
+                    className={`w-10 h-10 text-3xl cursor-pointer rounded-md  mx-auto ${
+                      reaction.reactors.includes(session?.user?.email || "")
+                        ? " bg-[#DC143C]"
+                        : "bg-gray-800"
+                    }`}
+                    onClick={() => addEmoji(reaction.emoji)}
+                  >
+                    {reaction.emoji}
+                  </div>
+                  <p className="text-white -mt-3 ml-6">
+                    {reaction.reactors.length}
+                  </p>
+                </span>
+              );
+            })}
         <MdOutlineAddCircle
           className="text-gray-200 bg-zinc-800 rounded-full w-10 h-10 p-1 cursor-pointer"
           onClick={() => setShowEmojiPicker(true)}
