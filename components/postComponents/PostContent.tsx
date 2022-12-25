0641ca1d524ab74d7ea1b7e8ce464c7050042681
zarif007 <zarifhuq786@gmail.com>
2022-12-26 01:22:54 +0600

    Adding author info for post

--- components/postComponents/PostContent.tsx
+++ components/postComponents/PostContent.tsx
@@ -1,106 +1,136 @@
 import axios from "axios";
-import Moment from 'react-moment';
+import Moment from "react-moment";
 import Head from "next/head";
 import React, { useState, useEffect } from "react";
 import postInterface from "../../Interfaces/PostInterface";
 import Slider from "../reusable/Slider";
 import PostReactions from "./PostReactions";
-import userInterface from '../../Interfaces/UserInterface';
-import LoadingSkeleton from '../reusable/LoadingSkeleton';
+import userInterface from "../../Interfaces/UserInterface";
+import LoadingSkeleton from "../reusable/LoadingSkeleton";
 import { apiEndpoints } from "../../domain";
-import { Tooltip } from '@material-tailwind/react';
-import { useRecoilState } from 'recoil';
+import { Tooltip } from "@material-tailwind/react";
+import { useRecoilState } from "recoil";
 import { creationModal } from "../../atoms/creationModal";
-import Image from 'next/image';
+import Image from "next/image";
 
 const PostContent: React.FC<{ post: postInterface }> = ({ post }) => {
-
-  const [openCreationModal, setOpenCreationModal] = useRecoilState(creationModal);
+  const [openCreationModal, setOpenCreationModal] =
+    useRecoilState(creationModal);
 
   const [authorInfo, setAuthorInfo] = useState<userInterface>({
-    name: '',
-    email: '',
-    image: '',
+    name: "",
+    email: "",
+    image: "",
   });
 
   useEffect(() => {
-    axios.get(`${apiEndpoints.user}/?email=${post.author}`)
-      .then(res => setAuthorInfo(res.data))
+    axios
+      .get(`${apiEndpoints.user}/?email=${post.author}`)
+      .then((res) => setAuthorInfo(res.data));
 
     setOpenCreationModal({ ...openCreationModal, post: false });
-  }, [])
-  
+  }, []);
+
   return (
     <div className="mx-2 md:mx-0">
       <Head>
         <title>{post.title}</title>
-        <meta name="description" content={post.content.substr(0, Math.min(20, post.content.length))} />
+        <meta
+          name="description"
+          content={post.content.substr(0, Math.min(20, post.content.length))}
+        />
         <link rel="icon" href="/favicon.ico" />
-        <meta property="og:image" content={post.images ? post.images[0] : authorInfo.image} />
+        <meta
+          property="og:image"
+          content={post.images ? post.images[0] : authorInfo.image}
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Oswald&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
-      {/* Image, Title and content  */}
-      <div className="bg-[#121212] border-2 border-[#DC143C] rounded-md">
-        {/* Title */}
-        <div className="rounded-md items-center">
-          <div className="text-2xl md:text-4xl font-bold text-[#DC143C] mx-4 my-3 cursor-pointer title">
-            {post.title}
-          </div>
-        </div>
-        
-        {
-          post.images && <div className="border-y-2 border-gray-900">
-            <Slider images={post.images} />
-          </div>
-        }
+      <div className="flex flex-row space-x-1">
+        {/* Post info */}
+        <div className="w-full lg:w-9/12">
+          <div className="bg-[#121212] border-2 border-[#DC143C] rounded-md ">
+            {/* Title */}
+            <div className="rounded-md items-center">
+              <div className="text-2xl md:text-4xl font-bold text-[#DC143C] mx-4 my-3 cursor-pointer title">
+                {post.title}
+              </div>
+            </div>
 
-        {/* Author Info  */}
-        <div className="m-2 mx-4">
-          {
-            authorInfo.email ?
-              <div className="flex space-x-2 items-center">
-                {
-                  authorInfo.image && <Image
-                    className="rounded-md"
-                    src={authorInfo.image}
-                    height={45}
-                    width={45}
-                    objectFit="cover"
-                    blurDataURL={authorInfo.image}
-                    placeholder="blur"
-                    alt="user DP"
-                  />
-                }
-                <div className="flex flex-col">
-                  <h1 className="text-sm text-gray-200">{authorInfo?.name}</h1>
-                  <h2 className="text-xs text-gray-500 font-semibold">
-                    <Tooltip content={`${post.createdAt.toString()}`} placement="bottom-start">
-                      <div>
-                        <Moment toNow ago>
-                          {post.createdAt}
-                        </Moment> <span> ago</span>
-                      </div>
-                    </Tooltip>
-                  </h2>
+            {post.images && (
+              <div className="border-y-2 border-gray-900">
+                <Slider images={post.images} />
+              </div>
+            )}
+
+            {/* Author Info  */}
+            <div className="m-2 mx-4">
+              {authorInfo.email ? (
+                <div className="flex space-x-2 items-center">
+                  {authorInfo.image && (
+                    <Image
+                      className="rounded-md"
+                      src={authorInfo.image}
+                      height={45}
+                      width={45}
+                      objectFit="cover"
+                      blurDataURL={authorInfo.image}
+                      placeholder="blur"
+                      alt="user DP"
+                    />
+                  )}
+                  <div className="flex flex-col">
+                    <h1 className="text-sm text-gray-200">
+                      {authorInfo?.name}
+                    </h1>
+                    <h2 className="text-xs text-gray-500 font-semibold">
+                      <Tooltip
+                        content={`${post.createdAt.toString()}`}
+                        placement="bottom-start"
+                      >
+                        <div>
+                          <Moment toNow ago>
+                            {post.createdAt}
+                          </Moment>{" "}
+                          <span> ago</span>
+                        </div>
+                      </Tooltip>
+                    </h2>
+                  </div>
                 </div>
-              </div> :
-              <LoadingSkeleton iteration={2} />
-          }
-        </div>
+              ) : (
+                <LoadingSkeleton iteration={2} />
+              )}
+            </div>
 
-        <div className="my-4 text-md md:text-lg font-semibold text-gray-300 mx-4 mb-8">
-          {post.content.split("\n").map((ps: string, index) => {
-            return <div key={index}>{ps}</div>;
-          })}
+            <div className="my-4 text-md md:text-lg font-semibold text-gray-300 mx-4 mb-8">
+              {post.content.split("\n").map((ps: string, index) => {
+                return <div key={index}>{ps}</div>;
+              })}
+            </div>
+          </div>
+          <PostReactions post={post} forTimeline={false} />
+        </div>
+        {/* Author and extra info */}
+        <div className="hidden lg:inline lg:w-3/12 rounded-md">
+          <div className="border-2 bg-[#121212] border-[#DC143C] rounded-md flex flex-col p-2 items-center">
+              <img className="rounded-full h-16 w-16" src={authorInfo.image} alt="author dp" />
+              <h1 className="text-white font-bold text-lg">{authorInfo.name}</h1>
+              <div className="my-2 flex text-gray-400 space-x-1">
+                <p>17 posts</p>
+                <p>/</p>
+                <p>350+ reactions</p>
+              </div>
+              <button className="my-2 py-1 w-full bg-[#DC143C] rounded-md font-semibold text-white">Subscribe</button>
+          </div>
         </div>
       </div>
-
-      
-      <PostReactions post={post} forTimeline={false} />
-      
     </div>
   );
 };
